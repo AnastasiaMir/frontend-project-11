5dd96fbb4e1461293b0e045c1c405bbea6db7f32
Anastasia Mironova <anastasia.mironova7@gmail.com>
2023-09-25 16:38:17 +0300

    eslint corrections

--- src/Example.js
+++ src/Example.js
@@ -1,10 +1,10 @@
 export default class Example {
-    constructor(element) {
-      this.element = element;
-    }
-  
-    init() {
-      this.element.textContent = 'hello, world!';
-      console.log('ehu!');
-    }
+  constructor(element) {
+    this.element = element;
   }
+
+  init() {
+    this.element.textContent = 'hello, world!';
+    console.log('ehu!');
+  }
+}
