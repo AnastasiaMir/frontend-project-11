a27489ee0f02a575fb59b0aa21daadfec6bedbed
Anastasia Mironova <anastasia.mironova7@gmail.com>
2023-11-26 13:56:45 +0300

    step 7. tests

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,7 +3,7 @@ export default {
     rssForm: {
       success: 'RSS успешно загружен',
       errors: {
-        notOneOf: 'Cсылка уже добавлена!',
+        notOneOf: 'RSS уже существует',
         url: 'Ссылка должна быть валидным URL!',
         rssNotValid: 'Ресурс не содержит валидный RSS',
         unknownError: 'Неизвестная ошибка!',
